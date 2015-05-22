5dde8d289483c848d67375420127270efe1cd8f5
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-05-22 13:17:38 +0200

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522110728) do
+ActiveRecord::Schema.define(version: 20150522111653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150522110728) do
   create_table "addresses", force: :cascade do |t|
     t.string   "city"
     t.string   "street"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
